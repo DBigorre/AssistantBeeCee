1c7e11f9992389dcb46f93217f6eb3a5962b0d03
desiree bigorre <bigorredesiree@gmail.com>
2022-03-29 10:12:51 -0400

    creation du new de l'admin pour question et link reliés

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :links, except: [:show]
   resources :questions, except: [:show]
+  get "questions/new_admin", to: "questions#new_admin"
 end
